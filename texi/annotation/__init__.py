632396b73e624d42e7e1f07473cf3deb96a673bd
Yevgnen Koh <wherejoystarts@gmail.com>
2021-01-21 15:44:56 +0800

    Add DualFileAnnotationParser

--- texi/annotation/__init__.py
+++ texi/annotation/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from texi.annotation.parser import AnnotationParser
+from texi.annotation.parser import AnnotationParser, DualFileAnnotationParser
 
 __all__ = [
     "AnnotationParser",
+    "DualFileAnnotationParser",
 ]
