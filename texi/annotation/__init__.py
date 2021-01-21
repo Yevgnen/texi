f773495c6ede2a79355ab3e100a5c78198147912
Yevgnen Koh <wherejoystarts@gmail.com>
2021-01-21 15:45:35 +0800

    Add BratParser

--- texi/annotation/__init__.py
+++ texi/annotation/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from texi.annotation.brat import BratParser
 from texi.annotation.parser import AnnotationParser, DualFileAnnotationParser
 
 __all__ = [
     "AnnotationParser",
     "DualFileAnnotationParser",
+    "BratParser",
 ]
