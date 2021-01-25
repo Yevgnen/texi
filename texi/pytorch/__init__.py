f9d4a36bff39285d8131f258cf648156c3afa512
Yevgnen Koh <wherejoystarts@gmail.com>
2021-01-25 22:43:20 +0800

    Add optim module

--- texi/pytorch/__init__.py
+++ texi/pytorch/__init__.py
@@ -6,6 +6,7 @@ import texi.pytorch.logger
 import texi.pytorch.losses
 import texi.pytorch.metrics
 import texi.pytorch.models
+import texi.pytorch.optim
 import texi.pytorch.rnn
 import texi.pytorch.scheduled_sampling
 import texi.pytorch.transformers
