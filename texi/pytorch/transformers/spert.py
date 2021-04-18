76b975e7fd78f8a0865cf3b119f625e9a1fa9110
Yevgnen Koh <wherejoystarts@gmail.com>
2021-04-18 17:54:48 +0800

    Extract methods

--- texi/pytorch/transformers/spert.py
+++ texi/pytorch/transformers/spert.py
@@ -342,23 +342,37 @@ class SpERTLoss(nn.Module):
         self.entity_loss = nn.CrossEntropyLoss(reduction="none")
         self.relation_loss = nn.BCEWithLogitsLoss(reduction="none")
 
-    def forward(
-        self,
-        entity_logits: torch.FloatTensor,
-        entity_labels: torch.LongTensor,
-        relation_logits: torch.FloatTensor,
-        relation_labels: torch.LongTensor,
-        entity_sample_masks: torch.LongTensor,
-        relation_sample_masks: torch.LongTensor,
-    ) -> torch.FloatTensor:
+    def _entity_loss(self, entity_logits, entity_labels, entity_sample_masks):
         entity_logits = entity_logits.transpose(-1, -2)
         entity_loss = self.entity_loss(entity_logits, entity_labels)
         entity_loss.masked_fill_(entity_sample_masks == 0, 0)
         entity_loss = entity_loss.sum() / entity_sample_masks.sum()
 
+        return entity_loss
+
+    def _relation_loss(self, relation_logits, relation_labels, relation_sample_masks):
         relation_loss = self.relation_loss(relation_logits, relation_labels.float())
         relation_loss.masked_fill_(relation_sample_masks.unsqueeze(-1) == 0, 0)
         relation_loss = relation_loss.sum() / relation_sample_masks.sum()
+
+        return relation_loss
+
+    def forward(
+        self,
+        entity_logits: torch.FloatTensor,
+        entity_labels: torch.LongTensor,
+        entity_sample_masks: torch.LongTensor,
+        relation_logits: torch.FloatTensor,
+        relation_labels: torch.LongTensor,
+        relation_sample_masks: torch.LongTensor,
+    ) -> torch.FloatTensor:
+        entity_loss = self._entity_loss(
+            entity_logits, entity_labels, entity_sample_masks
+        )
+
+        relation_loss = self._relation_loss(
+            relation_logits, relation_labels, relation_sample_masks
+        )
 
         loss = entity_loss + relation_loss
 
