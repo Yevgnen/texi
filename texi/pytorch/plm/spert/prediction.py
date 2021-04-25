d4ba00191833cc6389be07a7a165fc020979eab1
Yevgnen Koh <wherejoystarts@gmail.com>
2021-04-25 23:31:03 +0800

    Remove decode_relations

--- texi/pytorch/plm/spert/prediction.py
+++ texi/pytorch/plm/spert/prediction.py
@@ -7,44 +7,6 @@ import torch
 from texi.preprocessing import LabelEncoder
 
 
-def decode_relations(
-    relation_prob: torch.Tensor,
-    relation: torch.LongTensor,
-    relation_sample_mask: torch.LongTensor,
-    relation_label_encoder: LabelEncoder,
-    negative_relation_index: int,
-    relation_filter_threshold: float,
-) -> List[List[Dict[str, Any]]]:
-    relation_filter_mask = relation_prob < relation_filter_threshold
-    relation_pad_mask = ~relation_sample_mask.unsqueeze(dim=-1).bool()
-    mask = relation_pad_mask | relation_filter_mask
-    relation_prob = relation_prob.masked_fill(mask, -1)
-
-    if relation.size(1) > 0:
-        relation = relation.detach().cpu().numpy().tolist()
-        relation = [
-            [
-                {
-                    "type": relation_label_encoder.decode_label(
-                        k if label >= 0 else negative_relation_index
-                    ),
-                    "head": relation[i][j][0],
-                    "tail": relation[i][j][1],
-                }
-                for j, labels in enumerate(sample_labels)
-                for k, label in enumerate(labels)
-                if label >= 0
-            ]
-            for i, sample_labels in enumerate(
-                relation_prob.detach().cpu().numpy().tolist()
-            )
-        ]
-    else:
-        relation = [[] for _ in range(len(relation))]
-
-    return relation
-
-
 def predict_entities(
     entity_logit: torch.Tensor,
     entity_sample_mask: torch.LongTensor,
@@ -81,26 +43,46 @@ def predict_entities(
 
 
 def predict_relations(
-    relation_logit: torch.FloatTensor,
+    relation_logit: torch.Tensor,
     relation: torch.LongTensor,
     relation_sample_mask: torch.LongTensor,
     relation_label_encoder: LabelEncoder,
-    negative_relation_index: int,
     relation_filter_threshold: float,
 ) -> List[Dict[str, Any]]:
-    if relation_logit.size(1) > 0:
-        relation_predictions = decode_relations(
-            torch.sigmoid(relation_logit),
-            relation,
-            relation_sample_mask,
-            relation_label_encoder,
-            negative_relation_index,
-            relation_filter_threshold,
-        )
+    if relation_logit.dtype == torch.float32:
+        relation_proba = torch.sigmoid(relation_logit)
+    elif relation_logit.dtype == torch.int64:
+        relation_proba = relation_logit
     else:
-        relation_predictions = [[] for _ in range(len(relation))]
+        raise TypeError(
+            "`relation_proba` should `torch.int64` dtype when target is passed"
+            " or `torch.float32` dtype when logit is passed"
+        )
 
-    return relation_predictions
+    if relation.size(1) < 1:
+        return [[] for _ in range(len(relation))]
+
+    filter_mask = relation_proba < relation_filter_threshold
+    sample_mask = relation_sample_mask.unsqueeze(dim=-1).bool()
+    relation_proba.masked_fill_(~sample_mask | filter_mask, -1)
+
+    if relation.size(1) > 0:
+        pairs = relation.tolist()
+        relations = [
+            [
+                {
+                    "type": relation_label_encoder.decode_label(k),
+                    "head": pairs[i][j][0],
+                    "tail": pairs[i][j][1],
+                }
+                for j, labels in enumerate(sample_labels)
+                for k, label in enumerate(labels)
+                if label >= 0
+            ]
+            for i, sample_labels in enumerate(relation_proba.tolist())
+        ]
+
+    return relations
 
 
 def predict(
@@ -110,31 +92,63 @@ def predict(
     entity_label_encoder: LabelEncoder,
     negative_entity_index: int,
     relation_logit: torch.FloatTensor,
-    relation: torch.LongTensor,
+    relation_pair: torch.LongTensor,
     relation_sample_mask: torch.LongTensor,
     relation_label_encoder: LabelEncoder,
     negative_relation_index: int,
     relation_filter_threshold: float,
 ) -> List[Tuple[Dict[str, Any], Dict[str, Any]]]:
     # Predict entities.
-    entity_prediction = predict_entities(
+    entities = predict_entities(
         entity_logit,
         entity_mask,
         entity_span,
         entity_label_encoder,
-        negative_entity_index,
     )
-    for example_entity_prediction in entity_prediction:
-        example_entity_prediction.sort(key=lambda x: x["start"])
 
     # Predict relation.
-    relation_predictions = predict_relations(
+    relations = predict_relations(
         relation_logit,
-        relation,
+        relation_pair,
         relation_sample_mask,
         relation_label_encoder,
-        negative_relation_index,
         relation_filter_threshold,
     )
 
-    return list(zip(entity_prediction, relation_predictions))
+    negative_entity_label = entity_label_encoder.decode_label(negative_entity_index)
+    negative_relation_label = relation_label_encoder.decode_label(
+        negative_relation_index
+    )
+
+    def _normalize(entities, relations):
+        new_entities = []
+        entity_indices = {}
+        for i, entity in enumerate(entities):
+            if entity["type"] != negative_entity_label:
+                entity_indices[i] = len(entity_indices)
+                new_entities += [entity]
+
+        new_relations = [
+            {
+                "type": r["type"],
+                "head": entity_indices[r["head"]],
+                "tail": entity_indices[r["tail"]],
+            }
+            for r in relations
+            if r["type"] != negative_relation_label
+        ]
+
+        assert all(x["type"] != negative_entity_label for x in new_entities)
+        assert all(
+            x["type"] != negative_relation_label
+            and new_entities[x["head"]]["type"] != negative_entity_label
+            and new_entities[x["tail"]]["type"] != negative_entity_label
+            for x in new_relations
+        )
+
+        return new_entities, new_relations
+
+    return [
+        _normalize(sample_entities, sample_relations)
+        for sample_entities, sample_relations in zip(entities, relations)
+    ]
