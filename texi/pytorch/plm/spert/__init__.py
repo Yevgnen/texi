0403f83e1bda9ba9eb0b8a43952e69866f57fe7b
Yevgnen Koh <wherejoystarts@gmail.com>
2021-04-25 23:31:03 +0800

    Remove decode_relations

--- texi/pytorch/plm/spert/__init__.py
+++ texi/pytorch/plm/spert/__init__.py
@@ -4,7 +4,6 @@ from texi.pytorch.plm.spert.dataset import SpERTDataset
 from texi.pytorch.plm.spert.loss import SpERTLoss
 from texi.pytorch.plm.spert.model import SpERT
 from texi.pytorch.plm.spert.prediction import (
-    decode_relations,
     predict,
     predict_entities,
     predict_relations,
@@ -22,5 +21,4 @@ __all__ = [
     "predict",
     "predict_entities",
     "predict_relations",
-    "decode_relations",
 ]
