b737bfb17148ce441f111d21e888b1e50dcda433
Yevgnen Koh <wherejoystarts@gmail.com>
2021-05-23 11:27:37 +0800

    Fixed TextClassificationCollator

--- texi/pytorch/dataset/__init__.py
+++ texi/pytorch/dataset/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from texi.datasets import Dataset
-from texi.pytorch.dataset.classification import TextDataset, TextPairDataset
+from texi.pytorch.dataset.classification import (
+    TextClassificationCollator,
+    TextPairDataset,
+)
 from texi.pytorch.dataset.collator import Collator
 from texi.pytorch.dataset.question_answering import QuestionAnsweringDataset
 from texi.pytorch.dataset.sequence_labeling import SequenceLabelingDataset
@@ -9,7 +12,7 @@ from texi.pytorch.dataset.sequence_labeling import SequenceLabelingDataset
 __all__ = [
     "Collator",
     "Dataset",
-    "TextDataset",
+    "TextClassificationCollator",
     "TextPairDataset",
     "SequenceLabelingDataset",
     "QuestionAnsweringDataset",
