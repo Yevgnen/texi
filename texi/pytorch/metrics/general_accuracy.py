25a9171e023821a88e4fd67fd9066d6932152943
Yevgnen Koh <wherejoystarts@gmail.com>
2021-06-09 17:13:05 +0800

    Update GeneralAccuracy

--- texi/pytorch/metrics/general_accuracy.py
+++ texi/pytorch/metrics/general_accuracy.py
@@ -7,27 +7,22 @@ from ignite.metrics.metric import reinit__is_reduced, sync_all_reduce
 
 
 class GeneralAccuracy(Metric):
-    def __init__(self, output_transform=lambda x: x, device="cpu"):
-        self._num_correct = None
-        self._num_examples = None
-        super().__init__(output_transform=output_transform, device=device)
-
     @reinit__is_reduced
     def reset(self):
         self._num_correct = torch.tensor(0, device=self._device)
-        self._num_examples = 0
+        self._num_examples = torch.tensor(0, device=self._device)
         super().reset()
 
     @reinit__is_reduced
     def update(self, output):
-        if isinstance(output, dict):
-            y_pred, y = output["y_pred"], output["y"]
-        else:
-            y_pred, y = output
+        y_pred, y = output
+
+        y_pred = y_pred.detach()
+        y = y.detach()
 
         correct = [yi_pred == yi for yi_pred, yi in zip(y_pred, y)]
 
-        self._num_correct += torch.sum(correct).to(self._device)
+        self._num_correct += sum(correct)
         self._num_examples += len(correct)
 
     @sync_all_reduce("_num_examples", "_num_correct")
